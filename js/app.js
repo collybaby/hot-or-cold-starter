093f4e5050936d0d4b333bf6e4541ce65686917c
collybaby <colleengeorge111@gmail.com>
2016-03-02 20:23:54 -0800

    JS updates

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
 
 $(document).ready(function(){
+	var randomNumber = Math.floor((Math.random () * 100) +1);
+	var n = $("#guessButton").val();
+	n = parseInt (n);
+	n = math.abs();
+	$("#guessButton").val("");
+	$("#guessList").append("<li>" + n + "</li>" );
+		
+		
+
+		for ( ; ; ){
+	
+			if (randomNumber - n < 10) {
+				$("#feedback").append("<h2>" + "Cold" + "</h2")
+			}
+			else if (randomNumber - n > 10) {
+				$("#feedback").append("<h2>" + "Hot" + "</h2")
+			}
+			else (randomNumber - n == 0) {
+				$("#feedback").append("<h2>" + "You Got It!" + "</h2>")
+			}
+		}
+	})
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
